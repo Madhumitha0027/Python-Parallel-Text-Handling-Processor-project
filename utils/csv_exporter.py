9866618398a1952809e554661ece20e63787f50c
Madhumitha0027 <rayipallimadhu27@gmail.com>
2026-02-25 16:46:16 +0530

    Delete utils directory

--- utils/csv_exporter.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def export_to_csv(data, filename="output.csv"):
-    with open(filename,"w",newline='',encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(["id","text","score","tag","time"])
-        writer.writerows(data)
--- utils/email_sender.py
+++
@@ -1,2 +0,0 @@
-def send_email(receiver, subject, body):
-    print("Dummy email sent to", receiver)
