92230b41bce5599eb0755312df79ef8f86aad178
Madhumitha0027 <rayipallimadhu27@gmail.com>
2026-03-01 21:26:02 +0530

    Refactor main.py for multiprocessing and clarity

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ from database.db_manager import create_table, insert_result
 from modules.text_loader import split_text, parallel_process
 from modules.rule_engine import calculate_sentiment
 
-# 🔥 multiprocessing kosam TOP LEVEL function
 def process_chunk(chunk):
     score, tag = calculate_sentiment(chunk)
     insert_result(chunk, score, tag)
 
-# 🔥 VERY IMPORTANT (Windows multiprocessing fix)
+
 if __name__ == "__main__":
 
-    create_table()
-
-    # sample text (testing kosam)
+    create_table()  
     text = """
     Today is a good day. I feel happy.
     Sometimes coding gives error and makes me sad.
@@ -21,7 +18,6 @@ if __name__ == "__main__":
 
     chunks = split_text(text)
 
-    # 🔥 TRUE MULTIPROCESS EXECUTION
     parallel_process(chunks, process_chunk)
 
     print("Processing Completed!")
